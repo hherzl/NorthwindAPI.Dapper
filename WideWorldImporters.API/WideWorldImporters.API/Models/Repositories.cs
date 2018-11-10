88878a85c666581099aacc743b28e22655a437e0
HH <hans.herzl@live.com>
2018-11-10 02:31:19 -0600

    Addition of get all and get actions for API

---
+++ WideWorldImporters.API/WideWorldImporters.API/Models/Repositories.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace WideWorldImporters.API.Models
+{
+    public class AppSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+
+    public static class AppSettingsExtensions
+    {
+        public static IDbConnection GetConnection(this AppSettings appSettings)
+            => new SqlConnection(appSettings.ConnectionString);
+    }
+
+    public interface IWarehouseRepository
+    {
+        Task<IEnumerable<StockItem>> GetStockItemsAsync(int pageSize = 10, int pageNumber = 1, int? lastEditedBy = null, int? colorID = null, int? outerPackageID = null, int? supplierID = null, int? unitPackageID = null);
+
+        Task<StockItem> GetStockItemAsync(StockItem entity);
+
+        Task<StockItem> GetStockItemByStockItemNameAsync(StockItem entity);
+
+        Task<int> AddStockItemAsync(StockItem entity);
+
+        Task<int> UpdateStockItemAsync(StockItem entity);
+
+        Task<int> RemoveStockItemAsync(StockItem entity);
+    }
+
+    public abstract class Repository : IDisposable
+    {
+        protected bool Disposed;
+
+        public Repository(IDbConnection connection)
+        {
+            Connection = connection;
+        }
+
+        protected IDbConnection Connection { get; }
+
+        public void Dispose()
+        {
+            if (Disposed)
+                return;
+
+            Connection?.Dispose();
+
+            Disposed = true;
+        }
+    }
+
+    public class WarehouseRepository : Repository, IWarehouseRepository
+    {
+        public WarehouseRepository(IDbConnection connection)
+            : base(connection)
+        {
+        }
+
+        public async Task<IEnumerable<StockItem>> GetStockItemsAsync(int pageSize = 10, int pageNumber = 1, int? lastEditedBy = null, int? colorID = null, int? outerPackageID = null, int? supplierID = null, int? unitPackageID = null)
+        {
+            // Create string builder for query
+            var query = new StringBuilder();
+
+            // Create sql statement
+            query.Append(" select ");
+            query.Append("  [StockItemID], ");
+            query.Append("  [StockItemName], ");
+            query.Append("  [SupplierID], ");
+            query.Append("  [ColorID], ");
+            query.Append("  [UnitPackageID], ");
+            query.Append("  [OuterPackageID], ");
+            query.Append("  [Brand], ");
+            query.Append("  [Size], ");
+            query.Append("  [LeadTimeDays], ");
+            query.Append("  [QuantityPerOuter], ");
+            query.Append("  [IsChillerStock], ");
+            query.Append("  [Barcode], ");
+            query.Append("  [TaxRate], ");
+            query.Append("  [UnitPrice], ");
+            query.Append("  [RecommendedRetailPrice], ");
+            query.Append("  [TypicalWeightPerUnit], ");
+            query.Append("  [MarketingComments], ");
+            query.Append("  [InternalComments], ");
+            query.Append("  [CustomFields], ");
+            query.Append("  [Tags], ");
+            query.Append("  [SearchDetails], ");
+            query.Append("  [LastEditedBy], ");
+            query.Append("  [ValidFrom], ");
+            query.Append("  [ValidTo] ");
+            query.Append(" from ");
+            query.Append("  [Warehouse].[StockItems] ");
+            query.Append(" where ");
+            query.Append("  (@lastEditedBy is null or [LastEditedBy] = @lastEditedBy) and ");
+            query.Append("  (@colorID is null or [ColorID] = @colorID) and ");
+            query.Append("  (@outerPackageID is null or [OuterPackageID] = @outerPackageID) and ");
+            query.Append("  (@supplierID is null or [SupplierID] = @supplierID) and ");
+            query.Append("  (@unitPackageID is null or [UnitPackageID] = @unitPackageID)  ");
+            query.Append(" order by ");
+            query.Append("  [StockItemID] ");
+            query.Append(" offset @pageSize * (@pageNumber - 1) rows ");
+            query.Append(" fetch next @pageSize rows only ");
+
+            // Create parameters collection
+            var parameters = new DynamicParameters();
+
+            // Add parameters to collection
+            parameters.Add("@pageSize", pageSize);
+            parameters.Add("@pageNumber", pageNumber);
+            parameters.Add("@lastEditedBy", lastEditedBy);
+            parameters.Add("@colorID", colorID);
+            parameters.Add("@outerPackageID", outerPackageID);
+            parameters.Add("@supplierID", supplierID);
+            parameters.Add("@unitPackageID", unitPackageID);
+
+            // Retrieve result from database and convert to typed list
+            return await Connection.QueryAsync<StockItem>(new CommandDefinition(query.ToString(), parameters));
+        }
+
+        public async Task<StockItem> GetStockItemAsync(StockItem entity)
+        {
+            // Create string builder for query
+            var query = new StringBuilder();
+
+            // Create sql statement
+            query.Append(" select ");
+            query.Append("  [StockItemID], ");
+            query.Append("  [StockItemName], ");
+            query.Append("  [SupplierID], ");
+            query.Append("  [ColorID], ");
+            query.Append("  [UnitPackageID], ");
+            query.Append("  [OuterPackageID], ");
+            query.Append("  [Brand], ");
+            query.Append("  [Size], ");
+            query.Append("  [LeadTimeDays], ");
+            query.Append("  [QuantityPerOuter], ");
+            query.Append("  [IsChillerStock], ");
+            query.Append("  [Barcode], ");
+            query.Append("  [TaxRate], ");
+            query.Append("  [UnitPrice], ");
+            query.Append("  [RecommendedRetailPrice], ");
+            query.Append("  [TypicalWeightPerUnit], ");
+            query.Append("  [MarketingComments], ");
+            query.Append("  [InternalComments], ");
+            query.Append("  [CustomFields], ");
+            query.Append("  [Tags], ");
+            query.Append("  [SearchDetails], ");
+            query.Append("  [LastEditedBy], ");
+            query.Append("  [ValidFrom], ");
+            query.Append("  [ValidTo] ");
+            query.Append(" from ");
+            query.Append("  [Warehouse].[StockItems] ");
+            query.Append(" where ");
+            query.Append("  [StockItemID] = @stockItemID ");
+
+            // Create parameters collection
+            var parameters = new DynamicParameters();
+
+            // Add parameters to collection
+            parameters.Add("stockItemID", entity.StockItemID);
+
+            // Retrieve result from database and convert to entity class
+            return await Connection.QueryFirstOrDefaultAsync<StockItem>(query.ToString(), parameters);
+        }
+
+        public async Task<StockItem> GetStockItemByStockItemNameAsync(StockItem entity)
+        {
+            // Create string builder for query
+            var query = new StringBuilder();
+
+            // Create sql statement
+            query.Append(" select ");
+            query.Append("  [StockItemID], ");
+            query.Append("  [StockItemName], ");
+            query.Append("  [SupplierID], ");
+            query.Append("  [ColorID], ");
+            query.Append("  [UnitPackageID], ");
+            query.Append("  [OuterPackageID], ");
+            query.Append("  [Brand], ");
+            query.Append("  [Size], ");
+            query.Append("  [LeadTimeDays], ");
+            query.Append("  [QuantityPerOuter], ");
+            query.Append("  [IsChillerStock], ");
+            query.Append("  [Barcode], ");
+            query.Append("  [TaxRate], ");
+            query.Append("  [UnitPrice], ");
+            query.Append("  [RecommendedRetailPrice], ");
+            query.Append("  [TypicalWeightPerUnit], ");
+            query.Append("  [MarketingComments], ");
+            query.Append("  [InternalComments], ");
+            query.Append("  [CustomFields], ");
+            query.Append("  [Tags], ");
+            query.Append("  [SearchDetails], ");
+            query.Append("  [LastEditedBy], ");
+            query.Append("  [ValidFrom], ");
+            query.Append("  [ValidTo] ");
+            query.Append(" from ");
+            query.Append("  [Warehouse].[StockItems] ");
+            query.Append(" where ");
+            query.Append("  [StockItemName] = @stockItemName  ");
+
+            // Create parameters collection
+            var parameters = new DynamicParameters();
+
+            // Add parameters to collection
+            parameters.Add("stockItemName", entity.StockItemName);
+
+            // Retrieve result from database and convert to entity class
+            return await Connection.QueryFirstOrDefaultAsync<StockItem>(query.ToString(), parameters);
+        }
+
+        public async Task<int> AddStockItemAsync(StockItem entity)
+        {
+            // Create string builder for query
+            var query = new StringBuilder();
+
+            // Create sql statement
+            query.Append(" insert into ");
+            query.Append("  [Warehouse].[StockItems] ");
+            query.Append("  ( ");
+            query.Append("   [StockItemID], ");
+            query.Append("   [StockItemName], ");
+            query.Append("   [SupplierID], ");
+            query.Append("   [ColorID], ");
+            query.Append("   [UnitPackageID], ");
+            query.Append("   [OuterPackageID], ");
+            query.Append("   [Brand], ");
+            query.Append("   [Size], ");
+            query.Append("   [LeadTimeDays], ");
+            query.Append("   [QuantityPerOuter], ");
+            query.Append("   [IsChillerStock], ");
+            query.Append("   [Barcode], ");
+            query.Append("   [TaxRate], ");
+            query.Append("   [UnitPrice], ");
+            query.Append("   [RecommendedRetailPrice], ");
+            query.Append("   [TypicalWeightPerUnit], ");
+            query.Append("   [MarketingComments], ");
+            query.Append("   [InternalComments], ");
+            query.Append("   [CustomFields], ");
+            query.Append("   [Tags], ");
+            query.Append("   [SearchDetails], ");
+            query.Append("   [LastEditedBy], ");
+            query.Append("   [ValidFrom], ");
+            query.Append("   [ValidTo] ");
+            query.Append("  ) ");
+            query.Append(" values ");
+            query.Append(" ( ");
+            query.Append("  @stockItemID, ");
+            query.Append("  @stockItemName, ");
+            query.Append("  @supplierID, ");
+            query.Append("  @colorID, ");
+            query.Append("  @unitPackageID, ");
+            query.Append("  @outerPackageID, ");
+            query.Append("  @brand, ");
+            query.Append("  @size, ");
+            query.Append("  @leadTimeDays, ");
+            query.Append("  @quantityPerOuter, ");
+            query.Append("  @isChillerStock, ");
+            query.Append("  @barcode, ");
+            query.Append("  @taxRate, ");
+            query.Append("  @unitPrice, ");
+            query.Append("  @recommendedRetailPrice, ");
+            query.Append("  @typicalWeightPerUnit, ");
+            query.Append("  @marketingComments, ");
+            query.Append("  @internalComments, ");
+            query.Append("  @customFields, ");
+            query.Append("  @tags, ");
+            query.Append("  @searchDetails, ");
+            query.Append("  @lastEditedBy, ");
+            query.Append("  @validFrom, ");
+            query.Append("  @validTo ");
+            query.Append(" ) ");
+
+            // Create parameters collection
+            var parameters = new DynamicParameters();
+
+            // Add parameters to collection
+            parameters.Add("stockItemID", entity.StockItemID);
+            parameters.Add("stockItemName", entity.StockItemName);
+            parameters.Add("supplierID", entity.SupplierID);
+            parameters.Add("colorID", entity.ColorID);
+            parameters.Add("unitPackageID", entity.UnitPackageID);
+            parameters.Add("outerPackageID", entity.OuterPackageID);
+            parameters.Add("brand", entity.Brand);
+            parameters.Add("size", entity.Size);
+            parameters.Add("leadTimeDays", entity.LeadTimeDays);
+            parameters.Add("quantityPerOuter", entity.QuantityPerOuter);
+            parameters.Add("isChillerStock", entity.IsChillerStock);
+            parameters.Add("barcode", entity.Barcode);
+            parameters.Add("taxRate", entity.TaxRate);
+            parameters.Add("unitPrice", entity.UnitPrice);
+            parameters.Add("recommendedRetailPrice", entity.RecommendedRetailPrice);
+            parameters.Add("typicalWeightPerUnit", entity.TypicalWeightPerUnit);
+            parameters.Add("marketingComments", entity.MarketingComments);
+            parameters.Add("internalComments", entity.InternalComments);
+            parameters.Add("customFields", entity.CustomFields);
+            parameters.Add("tags", entity.Tags);
+            parameters.Add("searchDetails", entity.SearchDetails);
+            parameters.Add("lastEditedBy", entity.LastEditedBy);
+            parameters.Add("validFrom", entity.ValidFrom);
+            parameters.Add("validTo", entity.ValidTo);
+
+            // Execute query in database
+            return await Connection.ExecuteAsync(new CommandDefinition(query.ToString(), parameters));
+        }
+
+        public async Task<int> UpdateStockItemAsync(StockItem entity)
+        {
+            // Create string builder for query
+            var query = new StringBuilder();
+
+            // Create sql statement
+            query.Append(" update ");
+            query.Append("  [Warehouse].[StockItems] ");
+            query.Append(" set ");
+            query.Append("  [StockItemName] = @stockItemName, ");
+            query.Append("  [SupplierID] = @supplierID, ");
+            query.Append("  [ColorID] = @colorID, ");
+            query.Append("  [UnitPackageID] = @unitPackageID, ");
+            query.Append("  [OuterPackageID] = @outerPackageID, ");
+            query.Append("  [Brand] = @brand, ");
+            query.Append("  [Size] = @size, ");
+            query.Append("  [LeadTimeDays] = @leadTimeDays, ");
+            query.Append("  [QuantityPerOuter] = @quantityPerOuter, ");
+            query.Append("  [IsChillerStock] = @isChillerStock, ");
+            query.Append("  [Barcode] = @barcode, ");
+            query.Append("  [TaxRate] = @taxRate, ");
+            query.Append("  [UnitPrice] = @unitPrice, ");
+            query.Append("  [RecommendedRetailPrice] = @recommendedRetailPrice, ");
+            query.Append("  [TypicalWeightPerUnit] = @typicalWeightPerUnit, ");
+            query.Append("  [MarketingComments] = @marketingComments, ");
+            query.Append("  [InternalComments] = @internalComments, ");
+            query.Append("  [CustomFields] = @customFields, ");
+            query.Append("  [Tags] = @tags, ");
+            query.Append("  [SearchDetails] = @searchDetails, ");
+            query.Append("  [LastEditedBy] = @lastEditedBy, ");
+            query.Append("  [ValidFrom] = @validFrom, ");
+            query.Append("  [ValidTo] = @validTo ");
+            query.Append(" where ");
+            query.Append("  [StockItemID] = @stockItemID ");
+
+            // Create parameters collection
+            var parameters = new DynamicParameters();
+
+            // Add parameters to collection
+            parameters.Add("stockItemName", entity.StockItemName);
+            parameters.Add("supplierID", entity.SupplierID);
+            parameters.Add("colorID", entity.ColorID);
+            parameters.Add("unitPackageID", entity.UnitPackageID);
+            parameters.Add("outerPackageID", entity.OuterPackageID);
+            parameters.Add("brand", entity.Brand);
+            parameters.Add("size", entity.Size);
+            parameters.Add("leadTimeDays", entity.LeadTimeDays);
+            parameters.Add("quantityPerOuter", entity.QuantityPerOuter);
+            parameters.Add("isChillerStock", entity.IsChillerStock);
+            parameters.Add("barcode", entity.Barcode);
+            parameters.Add("taxRate", entity.TaxRate);
+            parameters.Add("unitPrice", entity.UnitPrice);
+            parameters.Add("recommendedRetailPrice", entity.RecommendedRetailPrice);
+            parameters.Add("typicalWeightPerUnit", entity.TypicalWeightPerUnit);
+            parameters.Add("marketingComments", entity.MarketingComments);
+            parameters.Add("internalComments", entity.InternalComments);
+            parameters.Add("customFields", entity.CustomFields);
+            parameters.Add("tags", entity.Tags);
+            parameters.Add("searchDetails", entity.SearchDetails);
+            parameters.Add("lastEditedBy", entity.LastEditedBy);
+            parameters.Add("validFrom", entity.ValidFrom);
+            parameters.Add("validTo", entity.ValidTo);
+            parameters.Add("stockItemID", entity.StockItemID);
+
+            // Execute query in database
+            return await Connection.ExecuteAsync(new CommandDefinition(query.ToString(), parameters));
+        }
+
+        public async Task<int> RemoveStockItemAsync(StockItem entity)
+        {
+            // Create string builder for query
+            var query = new StringBuilder();
+
+            // Create sql statement
+            query.Append(" delete from ");
+            query.Append("  [Warehouse].[StockItems] ");
+            query.Append(" where ");
+            query.Append("  [StockItemID] = @stockItemID ");
+
+            // Create parameters collection
+            var parameters = new DynamicParameters();
+
+            // Add parameters to collection
+            parameters.Add("stockItemID", entity.StockItemID);
+
+            // Execute query in database
+            return await Connection.ExecuteAsync(new CommandDefinition(query.ToString(), parameters));
+        }
+    }
+}
--- WideWorldImporters.API/WideWorldImporters.API/Startup.cs
+++ WideWorldImporters.API/WideWorldImporters.API/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using WideWorldImporters.API.Controllers;
+using WideWorldImporters.API.Models;
 
 namespace WideWorldImporters.API
 {
@@ -26,21 +22,26 @@ namespace WideWorldImporters.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Setting up dependency injection
+
+            // AppSettings
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            services.AddScoped<AppSettings>();
+
+            // Logging for controller
+            services.AddScoped<ILogger, Logger<WarehouseController>>();
+
+            // Repository
+            services.AddScoped<IWarehouseRepository, WarehouseRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
